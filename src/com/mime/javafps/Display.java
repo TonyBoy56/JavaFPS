42ac543fdea62c2e8f74153c5fa42aa5934e63dd
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-08-23 15:46:02 -0700

    add call to .setDefaultCloseOperation to terminate the window upon close.

--- src/com/mime/javafps/Display.java
+++ src/com/mime/javafps/Display.java
@@ -4,9 +4,11 @@ import java.awt.Canvas;
 import javax.swing.JFrame;
 
 public class Display extends Canvas {
+	private static final long serialVersionUID = 1L;
 	
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
+	public static final String TITLE = "JavaFPS Pre-Alpha 0.01";
 	
 //	main is needed to run the program
 //	main will produce a fixed-frame
@@ -14,8 +16,15 @@ public class Display extends Canvas {
 		Display game = new Display();
 		JFrame frame = new JFrame();
 		frame.add(game);
+
+		//the window will terminate itself after closing the window		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setTitle(TITLE);
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setResizable(false);
 		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		System.out.println("We're still running...");
 	}
 }
